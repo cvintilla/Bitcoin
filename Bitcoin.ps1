89a8ca38ee415d6829644dcbc147a05f8a76f0ab
cvintilla <c.vintilla@gmail.com>
2018-12-02 17:15:39 -0500

    Update Bitcoin.ps1

--- Bitcoin.ps1
+++ Bitcoin.ps1
@@ -43,8 +43,7 @@ while (1 -eq 1) {
 	Clear-Host
 	$data = Invoke-WebRequest -Uri https://api.coinbase.com/v2/prices/BTC-USD/sell -Method Get
 	$object = $data | ConvertFrom-Json
-	Write-Host $object.data.base
-	Write-Host $object.data.currency
-	Write-Host $object.data.amount
+	Write-Host ('1 ' + $object.data.base)
+	Write-Host ('$' + $object.data.amount)
 	Start-Sleep -s $sec
 }
